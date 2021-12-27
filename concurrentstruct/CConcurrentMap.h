ecc3a1cb27d9fe23c3308f238831308fa66502c4
dnikiforov1967 <dnikiforov1967@gmail.com>
2021-12-27 17:24:34 +0100

    Append comparator and allocator template parameters

--- concurrentstruct/CConcurrentMap.h
+++ concurrentstruct/CConcurrentMap.h
@@ -10,9 +10,11 @@ Author D.Nikiforov, 12.2021
 #include <map>
 #include <mutex>
 
-template <typename _Key, typename _Tp> class CMapNode {
+template <typename _Key, typename _Tp, 
+	typename _Compare = std::less<_Key>,
+	typename _Allocator = std::allocator<std::pair<const _Key, _Tp>>> class CMapNode {
 	std::mutex mutex_;
-	std::map<_Key, _Tp> _storage;
+	std::map<_Key, _Tp, _Compare, _Allocator> _storage;
 public:
 	CMapNode();
 	CMapNode(const  CMapNode&) = delete;
@@ -21,18 +23,25 @@ public:
 	const _Tp* get(const _Key&);
 };
 
-template <typename _Key, typename _Tp> CMapNode<_Key, _Tp>::CMapNode() {
+template <typename _Key, typename _Tp, 
+	typename _Compare,
+	typename _Allocator>
+	CMapNode<_Key, _Tp, _Compare, _Allocator>::CMapNode() {
 }
 
-template <typename _Key, typename _Tp> 
-_Tp& CMapNode<_Key, _Tp>::put(const _Key key, const _Tp val) {
+template <typename _Key, typename _Tp, 
+	typename _Compare,
+	typename _Allocator>
+	_Tp& CMapNode<_Key, _Tp, _Compare, _Allocator>::put(const _Key key, const _Tp val) {
 	const std::lock_guard<std::mutex> lock(mutex_);
 	_storage[key] = val;
 	return _storage[key];
 }
 
-template <typename _Key, typename _Tp> 
-const _Tp* CMapNode<_Key, _Tp>::get(const _Key& key) {
+template <typename _Key, typename _Tp,
+	typename _Compare,
+	typename _Allocator>
+	const _Tp* CMapNode<_Key, _Tp, _Compare, _Allocator>::get(const _Key& key) {
 	const std::lock_guard<std::mutex> lock(this->mutex_);
 	const _Tp* ptr = nullptr;
 	if (auto found = _storage.find(key); found != _storage.end())

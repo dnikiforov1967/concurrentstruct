f03880cd1e097d0918f3ab744b1262c46efc0d2d
dnikiforov1967 <dnikiforov1967@gmail.com>
2021-12-30 20:45:07 +0100

    Modify storage and method return types

--- concurrentstruct/CConcurrentMap.h
+++ concurrentstruct/CConcurrentMap.h
@@ -7,70 +7,42 @@ Author D.Nikiforov, 12.2021
 #ifndef _CONCURRENT_MAP_
 #define _CONCURRENT_MAP_
 
-#include <map>
+#include <set>
 #include <mutex>
 #include <unordered_map>
 
 namespace util {
 
-	template <typename _Key, typename _Tp,
-		typename _Compare = std::less<_Key>,
-		typename _Allocator = std::allocator<std::pair<const _Key, _Tp>>> class map_node {
+	template <typename _Key, typename _Compare = std::less<_Key>> class map_node {
 		std::mutex mutex_;
-		std::map<_Key, _Tp, _Compare, _Allocator> _storage;
+		std::set<_Key, _Compare> _storage;
 		public:
 			map_node();
 			map_node(const map_node&) = delete;
 			map_node& operator=(const map_node&) = delete;
-			_Tp& put(const _Key, const _Tp);
-			const _Tp* get(const _Key&);
+			const _Key* put(const _Key&);
+			const _Key* get(const _Key&);
 	};
 
-	template <typename _Key, typename _Tp,
-		typename _Compare,
-		typename _Allocator>
-		map_node<_Key, _Tp, _Compare, _Allocator>::map_node() {
+	template <typename _Key, typename _Compare> map_node<_Key, _Compare>::map_node() {
 	}
 
-	template <typename _Key, typename _Tp,
-		typename _Compare,
-		typename _Allocator>
-		_Tp& map_node<_Key, _Tp, _Compare, _Allocator>::put(const _Key key, const _Tp val) {
+
+	template <typename _Key, typename _Compare> const _Key* map_node< _Key, _Compare>::put(const _Key& key) {
 		const std::lock_guard<std::mutex> lock(mutex_);
-		_storage[key] = val;
-		return _storage[key];
+		_storage.emplace(key);
+		auto found = _storage.find(key);
+		return &(*found);
 	}
 
-	template <typename _Key, typename _Tp,
-		typename _Compare,
-		typename _Allocator>
-		const _Tp* map_node<_Key, _Tp, _Compare, _Allocator>::get(const _Key& key) {
+	
+	template <typename _Key, typename _Compare> const _Key* map_node< _Key, _Compare>::get(const _Key& key) {
 		const std::lock_guard<std::mutex> lock(this->mutex_);
-		const _Tp* ptr = nullptr;
+		const int* ptr = nullptr;
 		if (auto found = _storage.find(key); found != _storage.end())
-			ptr = &(found->second);
+			ptr = &(*found);
 		return ptr;
 	}
-
-	template <typename _Key, typename _Tp> class concurrent_map {
-		int concurrency_level;
-		std::unordered_map<int, map_node<_Key, _Tp>> _storege;
-	public:
-		concurrent_map();
-		explicit concurrent_map(int);
-	};
-
-	template <typename _Key, typename _Tp>
-	concurrent_map<_Key, _Tp>::concurrent_map()
-		: concurrency_level{ 4 } {
-	}
-
-	template <typename _Key, typename _Tp>
-	concurrent_map<_Key, _Tp>::concurrent_map(int level)
-		: concurrency_level{ level } {
-	}
-
-
 
 }
 
